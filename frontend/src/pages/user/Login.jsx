f9efd997717ba774a45f08093ac0a2de290a20ee
Sathmi1234 <devsadini@gmail.com>
2024-07-02 12:05:31 +0530

    commit4 day34 (fixing login and google login)

--- frontend/src/pages/user/Login.jsx
+++ frontend/src/pages/user/Login.jsx
@@ -5,76 +5,71 @@ import { IoEyeOutline } from "react-icons/io5";
 import GoogleLogin from '../../components/Social/GoogleLogin';
 import useAuth from '../../hooks/useAuth';
 import Swal from 'sweetalert2';
-const Login = () => {
 
+const Login = () => {
   const [showPassword, setShowPassword] = useState(false)
 
   const location = useLocation();
-  const {login,error,setError,loader,setLoader} = useAuth();
+  const { login, error, setError, loader, setLoader } = useAuth();
 
   const navigate = useNavigate();
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('')   
+    setError('')
 
     const data = new FormData(e.target);
-    const formData = Object.fromEntries(data)
-    try{
-      const user = await login(formData.email,formData.password).then((res)=>{
+    const formData = Object.fromEntries(data);
+
+    try {
+      setLoader(true);
+      const user = await login(formData.email, formData.password);
+      
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Login Successful!!",
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+      navigate(location.state?.from || '/', { state: { user } });
+
+    } catch (error) {
+      console.log(error);
+      setError(error.code);
+
+      if (!formData.email && formData.password || !formData.password && formData.email) {
         Swal.fire({
           position: "top-end",
-          icon: "success",
-          title: "Login Successful!!",
-          showConfirmButton: false,
-          timer: 1500
-        });
-        navigate(location.state?.from || '/', {state:{user}})
-
-      }).catch((error)=>{
-        Swal.fire({
-          position: "top-end", 
           icon: "warning",
-          title: "Give correct Email and password <br/> OR <br/> use Google Login",
+          title: "Fill all the fields",
           showConfirmButton: false,
           timer: 1500,
+          
+        });
+      } else {
+        Swal.fire({
+          position: "top-end",
+          icon: "warning",
+          title: "Give correct Email and password <br/> OR <br/> use Google",
+          showConfirmButton: false,
+          timer: 1000,
           customClass: {
             popup: 'swal2-custom-popup'
           },
           didOpen: () => {
             const popup = document.querySelector('.swal2-custom-popup');
             if (popup) {
-              popup.style.width = '400px'; 
-              popup.style.height = '300px';
-              popup.style.fontSize = '0.8em';
+              popup.style.width = '400px'; // Adjust the width
+              popup.style.height = '300px'; // Adjust the height
+              popup.style.fontSize = '0.8em'; // Adjust the font size
             }
           }
         });
-        
-      })
-
-    }catch(err){
-      setError(err.code);
-      if((!formData.email && formData.password) || (formData.email && !formData.password)){
-        Swal.fire({
-          position: "top-end",
-          icon: "warning",
-          title: "Fill all the fields",
-          showConfirmButton: false,
-          timer: 1000
-        });
-      }else{
-        Swal.fire({
-          position: "top-end",
-          icon: "warning",
-          title: "Error Login",
-          showConfirmButton: false,
-          timer: 1000
-        });
-
       }
-      
-    }finally{
+
+    } finally {
       setLoader(false);
     }
   }
@@ -89,26 +84,25 @@ const Login = () => {
           <div>
             <label htmlFor='email' className='sr-only'>Email</label>
             <div className='relative'>
-              <input type='email' name='email' placeholder='Enter Email' className='w-full text-center border outline-none rounded-lg border-gray-200 pe-12 text-lg shadow-sm h-10'/>
-              <span className='absolute inset-y-0 end-0 grid place-content-center px-4'><MdAlternateEmail className='h-4 w-4 text-gray-400'/></span>
+              <input type='email' name='email' placeholder='Enter Email' className='w-full text-center border outline-none rounded-lg border-gray-200 pe-12 text-lg shadow-sm h-10' />
+              <span className='absolute inset-y-0 end-0 grid place-content-center px-4'><MdAlternateEmail className='h-4 w-4 text-gray-400' /></span>
             </div>
           </div>
 
           <div>
             <label htmlFor='password' className='sr-only'>Password</label>
             <div className='relative'>
-              <input type={showPassword ? 'text' :'password'} name='password' placeholder='Enter Password' className='w-full text-center border outline-none rounded-lg border-gray-200 pe-12 text-lg shadow-sm h-10'/>
-              <span onClick={()=>setShowPassword(!showPassword)} className='absolute inset-y-0 end-0 grid place-content-center px-4'><IoEyeOutline  className='h-4 w-4 text-gray-400'/></span>
+              <input type={showPassword ? 'text' : 'password'} name='password' placeholder='Enter Password' className='w-full text-center border outline-none rounded-lg border-gray-200 pe-12 text-lg shadow-sm h-10' />
+              <span onClick={() => setShowPassword(!showPassword)} className='absolute inset-y-0 end-0 grid place-content-center px-4'><IoEyeOutline className='h-4 w-4 text-gray-400' /></span>
             </div>
           </div>
           <button type='submit' className='block w-full rounded-lg bg-secondary px-5 py-3 font-medium text-white text-lg'>Sign-in</button>
           <p className='text-center text-sm text-gray-500'>No account? <Link className='underline text-secondary' to="/register">Sign-up</Link></p>
-          <GoogleLogin/>
+          <GoogleLogin />
         </form>
-
       </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
